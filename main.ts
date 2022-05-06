7e9b4d560fa81d704b6e2387831daffc8e7340d8
1812100 <105028872+1812100@users.noreply.github.com>
2022-05-06 10:13:53 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("Hello!")
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("my name is Howince")
+    basic.showString("my name is James bone 007")
 })
 basic.forever(function () {
 	
