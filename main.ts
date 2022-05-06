b47ff61cbbe87c902bcc2ff543018bb378184a27
1812100 <105028872+1812100@users.noreply.github.com>
2022-05-06 10:15:24 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString("Hello!")
-})
 input.onButtonPressed(Button.B, function () {
     basic.showString("my name is James bone 007")
 })
